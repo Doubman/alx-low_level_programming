f52818bcb85bc4d7dcf4874a220650ed3aff3473
Doubman <Doubaraogori@icloud.com>
2023-07-13 15:06:20 -0700

    updated puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(void)
 {
-	/*printing a string without printf,using puts*/
-	char firstString[] = "\"Programming is like building a multilingual puzzle";
- 	puts(firstString);
- 	return 0;
+/*printing a string without printf,using puts*/
+char firstString[] = "\"Programming is like building a multilingual puzzle";
+puts(firstString);
+return (0);
 }
