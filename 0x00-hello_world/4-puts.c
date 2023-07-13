273881661eab624f66168206e8b542396c69b83c
Doubman <Doubaraogori@icloud.com>
2023-07-13 14:41:40 -0700

    put command script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
-#!/bin/bash
-puts(char "Programming is like building a multilingual puzzle/n");
-return 0;
+#include<stdio.h>
+int main()
+{
+	/*printing a string without printf,using puts*/
+    char firstString[]= "Programming is like building a multilingual puzzle/n";
+    
+    puts(firstString);
+    
+    return 0;
+}
