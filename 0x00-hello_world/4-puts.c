28079b5452b20d90d9e9b92d41c55178bd2e0ceb
Doubman <Doubaraogori@icloud.com>
2023-07-13 14:55:21 -0700

    fixed script to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
 	/*printing a string without printf,using puts*/
-    char firstString[]= "Programming is like building a multilingual puzzle";
-    
-    puts(firstString);
-    
-    return 0;
+	char firstString[] = "\"Programming is like building a multilingual puzzle";
+ 	puts(firstString);
+ 	return 0;
 }
