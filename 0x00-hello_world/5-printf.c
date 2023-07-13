ce842c5bc08767138ef610b6beb85de7986cd13a
Doubman <Doubaraogori@icloud.com>
2023-07-13 16:31:22 -0700

    printf script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * Function to print a string
+ */
+int main(void)
+{
+/* This function outputs a string using printf*/
+printf("with proper grammar, but the outcome is a piece of art,\n");
+/* Returns a value of 0 */
+return (0);
+}
