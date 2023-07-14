39f5db9b4c984555867c31ba29d4e4fa28ed0f8d
Doubman <Doubaraogori@icloud.com>
2023-07-13 17:14:27 -0700

    print script update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,4 +9,5 @@ int main(void)
 /* printf - outputs a string */
 printf("with proper grammar, but the outcome is a piece of art,\n");
 /* return -Returns a value of 0 */
-return (0); 
+return (0);
+}
