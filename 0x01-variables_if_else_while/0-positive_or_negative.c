6dc2a549c355d8befd0bbee54ebd54d68d9ebd45
Doubman <Doubaraogori@icloud.com>
2023-07-14 15:01:02 -0700

    fixed errors in positive - negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -15,7 +16,7 @@ int main(void)
 	{
 		printf("%d is postive\n", n);
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
