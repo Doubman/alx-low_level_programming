67392fd1ae0568e55e0ffeec609ed33457114d00
Doubman <Doubaraogori@icloud.com>
2023-07-18 19:07:33 -0700

    used putchar to print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+/**
+  * main - Entry point
+  *
+  *
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int txt = "_putchar";
+	putchar(txt);
+	putchar(\n);
+
+	    return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Series of programs showing use of functions and nested loops in C.
